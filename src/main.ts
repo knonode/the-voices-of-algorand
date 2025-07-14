45c6bee6992c2b73fa8433f3fb5e5dd019e5f909
knonode <algohmp@gmail.com>
2025-07-14 02:05:05 +0200

    vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { EChartsService } from './echartsService';
 import { PopularityChartService } from './popularityChartService';
 import { StatisticsService } from './statisticsService';
 import { Candidate, Vote } from './types';
-import { Analytics } from "@vercel/analytics/next"
 import * as echarts from 'echarts';
 import confetti from 'canvas-confetti';
 
